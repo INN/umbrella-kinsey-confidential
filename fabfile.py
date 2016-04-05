bb9dda5bf3cab166c18a7616ac8839328ae13737
Ben Keith <ben@benlk.com>
2016-04-05 16:59:01 -0400

    fabfile production variables

--- fabfile.py
+++ fabfile.py
@@ -1,13 +1,19 @@
+import os
 from tools.fablib import *
-
 from fabric.api import task
-
+from fabric.colors import red
 
 """
 Base configuration
 """
 env.project_name = 'kinseycon'       # name for the project.
 env.hosts = ['localhost', ]
+
+try:
+    env.domain
+except AttributeError:
+    env.domain = 'vagrant.dev'
+
 
 """
 Add HipChat info to send a message to a room when new code has been deployed.
@@ -23,9 +29,11 @@ def production():
     Work on production environment
     """
     env.settings    = 'production'
-    env.hosts       = []    # ssh host for production.
-    env.user        = ''    # ssh user for production.
-    env.password    = ''    # ssh password for production.
+    env.hosts       = [os.environ['KINSEY_CON_PRODUCTION_SFTP_HOST'], ]    # ssh host for production.
+    env.user        = os.environ['KINSEY_CON_PRODUCTION_SFTP_USER']    # ssh user for production.
+    env.password    = os.environ['KINSEY_CON_PRODUCTION_SFTP_PASSWORD']    # ssh password for production.
+    env.domain      = 'kinseycon.wpengine.com'
+    env.port        = 2222
 
 
 @task
@@ -37,6 +45,10 @@ def staging():
     env.hosts       = []    # ssh host for staging.
     env.user        = ''    # ssh user for staging.
     env.password    = ''    # ssh password for staging.
+    env.domain      = ''
+    env.port        = 2222
+
+    print(red("This project does not have a staging environment configured!"))
 
 try:
     from local_fabfile import  *
